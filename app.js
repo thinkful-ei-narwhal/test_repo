53d8c771b94b246104121809c081b604a07eec7f
Orkun Ozekin <orkunozekin@thinkful.com>
2020-03-16 15:44:06 -0500

    First Function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 /*let myTeam = 'Max, Orkun';
 console.log(myTeam);*/
 
+function createGreeting() {
 let englStatement = "Hi, my name is Chris and I\'m 29 years old.";
 
 console.log(englStatement);
+}
+
+createGreeting();
