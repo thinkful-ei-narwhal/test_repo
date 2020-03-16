55bfe16ad8686c201078b131b35b8b52d34b56f0
Orkun Ozekin <orkunozekin@thinkful.com>
2020-03-16 16:17:33 -0500

    YOB

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 /*let myTeam = 'Max, Orkun';
 console.log(myTeam);*/
 
+function getYearOfBirth(age) {
+     return 2020 - age;
+}
+
+
+
 function createGreeting(name, age) {
 let englStatement = "Hi, my name is Chris and I\'m 29 years old.";
-const yearOfBirth = 2020 - age;
-return `I was born in ${yearOfBirth}`
+const yob = getYearOfBirth(21);
+return `I was born in ${yob}`;
+
 }
 
 const greeting1 = createGreeting('orkun', 24);
 console.log(greeting1);
 
+
+const saveFunction = getYearOfBirth
