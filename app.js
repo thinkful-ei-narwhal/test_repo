40355bc678690aec56e88a0c5bc3c4c643756308
Orkun Ozekin <orkunozekin@thinkful.com>
2020-03-16 16:38:09 -0500

    if age is negative

--- app.js
+++ app.js
@@ -14,8 +14,17 @@ return `I was born in ${yob}`;
 
 }
 
+function yearOfBirth(age) {
+    if(age < 0) {
+        throw new Error("Age can not be negative");
+    }
+}
+
+
 const greeting1 = createGreeting('orkun', 24);
 console.log(greeting1);
 
 
 const saveFunction = getYearOfBirth
+
+yearOfBirth(-5);
